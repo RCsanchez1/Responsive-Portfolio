f8eec3433e0d99527493e4b4de1b2d8b5f3a27fb
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-10-21 22:27:26 -0600

    added components file

--- portfolio/src/styles.js
+++ portfolio/src/styles.js
@@ -11,3 +11,13 @@ export const Paragraph = styled.p`
 `;
 
 
+export const Pill = styled.span`
+  display: inline-block;
+  margin-right: 0.75rem;
+  margin-bottom: 0.75rem;
+  padding: 0.5rem 1rem;
+  background-color: ${blue[20]};
+  color: ${blue[70]};
+  border-radius: 2px;
+  font-weight: bold;
+`;
