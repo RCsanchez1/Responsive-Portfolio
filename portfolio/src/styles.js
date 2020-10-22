d359f2dcce8908837bf110663fea868dd7d8e547
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-10-21 21:52:17 -0600

    added code to style.js

---
+++ portfolio/src/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { blue } from '@carbon/colors';
+
+export const SectionTitle = styled.h3`
+  margin-top: 1.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const Paragraph = styled.p`
+  white-space: pre-wrap;
+`;
+
+
